dd3304d72e949ac4a126b7594cfad4c4fb46bb65
dragonsearch <javmalla1@gmail.com>
2024-08-11 04:33:43 +0200

    update: restructure of table loading millsong

--- sql_loading_table/millsong.sql
+++ sql_loading_table/millsong.sql
@@ -2,11 +2,12 @@ SET GLOBAL local_infile = true;
 CREATE DATABASE IF NOT EXISTS spotify;
 USE spotify;
 CREATE TABLE millsong(
-    id INT PRIMARY KEY,
-    artist varchar(255),
-    song varchar(255),
-    link varchar(255),
-    text varchar(8191)
+    id INT,
+    track_artist varchar(255) NOT NULL,
+    track_name varchar(255) NOT NULL,
+    link varchar(255) NOT NULL,
+    lyrics varchar(8191) NOT NULL,
+    PRIMARY KEY (track_artist, track_name)
 );
 
 SELECT * FROM millsong;
@@ -14,10 +15,4 @@ LOAD DATA LOCAL INFILE './data/spotify_millsongdata_%.csv' INTO TABLE millsong
 FIELDS TERMINATED BY '%'
 IGNORE 1 LINES;
 
-DELETE FROM millsong 
-WHERE artist IS NULL
-OR song IS NULL
-OR link IS NULL
-OR text IS NULL;
-
-SELECT artist FROM millsong
+SELECT track_artist FROM millsong LIMIT 10;
